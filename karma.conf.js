df240008d77565eae809d51535bd9f9d45fb3449
Subhash <85936425+Subhash30d@users.noreply.github.com>
2023-01-09 10:49:11 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ReactiveFormsTemplate'),
+      dir: require('path').join(__dirname, './coverage/assignments-proj'),
       subdir: '.',
       reporters: [
         { type: 'html' },
